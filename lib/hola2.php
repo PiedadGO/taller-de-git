76b7a1482eaa14ed65c086c91a96d1d36909b2d9
Piedad García <piedad.pgo@gmail.com>
2023-10-11 20:58:15 +0200

    hola usa la clase HolaMundo

--- lib/hola2.php
+++ lib/hola2.php
@@ -1,6 +1,10 @@
 <?php
 // Autor: Piedad García <pgarort491@g.educaand.es>
-//El nombre por defecto es Mundo 
-$nombre =isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";
+// El nombre por defecto es Mundo
+require('HolaMundo.php');
+
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print new HolaMundo($nombre);
+
+
 
