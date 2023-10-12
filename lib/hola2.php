eee1cca1bf8b68c2727613509f529eb6277eeb41
Piedad García <piedad.pgo@gmail.com>
2023-10-12 19:23:46 +0200

    Arreglando confictos de merge

--- lib/hola2.php
+++ lib/hola2.php
@@ -1,8 +1,8 @@
 <?php
 // Autor: Piedad García <pgarort491@g.educaand.es>
+
 // El nombre por defecto es Mundo
 require('HolaMundo.php');
-
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 print new HolaMundo($nombre);
 
